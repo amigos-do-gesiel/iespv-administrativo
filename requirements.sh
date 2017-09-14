c7e51fc47858183ba6e95fe3fb439edb3a437b9c
Anderson Silva <andersoonn.ams@gmail.com>
2017-09-13 23:37:41 -0300

    Fixing Vangrantfile

--- requirements.sh
+++ requirements.sh
@@ -1,8 +1,10 @@
+sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install -y libenchant1c2a
 sudo apt-get install -y sqlite3
-sudo apt-get install python3=3.5
+#sudo apt-get install -y python3
+sudo apt-get install -y python3.5
 
 sudo apt-get install -y python-pip
 sudo apt-get install -y python3-pip
@@ -13,9 +15,18 @@ sudo apt-get install -y libjpeg-dev
 sudo apt-get install -y libjpeg8-dev
 sudo apt-get install -y libpng-dev
 
+sudo pip3 install Django==1.9.8
+sudo pip3 install django-bootstrap4
+sudo pip3 install unipath
+sudo pip3 install Pillow
+sudo pip3 install django-crispy-forms
+sudo pip3 install pusher
+sudo pip3 install pytest
+sudo pip3 install pytest-django
 
 
-sudo pip3 install -r project/IESPV/requirements.txt
+
+#sudo pip3 install -r project/IESPV/requirements.txt not working, i dont know why :(
 
 #sudo pip3 install --upgrade urllib3
 #sudo apt-get -y install build-essential libssl-dev libffi-dev python3-dev

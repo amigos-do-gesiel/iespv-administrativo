28b57d20f809639b897d2ed0f28b60566f20ebfc
ulyssesBML <ulysses3353@gmail.com>
2017-09-15 17:13:39 -0300

    create model Recovery password and methods of recovery and templates of user
    Signed-off-by: ulyssesBML <ulysses3353@gmail.com>
    Signed-off-by: GesielFreitas <gesiel.was.f@gmail.com>

--- project/IESPV/IESPV/urls.py
+++ project/IESPV/IESPV/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^users/', include('users.urls')),
 ]

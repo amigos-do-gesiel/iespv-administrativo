8a2e221fe2d0e4a7a33fb3afef8b3b00cfe492e4
Marco Kuroiva <marco.antoniorl10@gmail.com>
2017-09-17 20:29:05 -0300

    Stablishing routes and defining users index page

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Employee(User):
 
     abstract = True
-    access_level
+    access_level = 0
 
     def register_donator():
         pass
@@ -31,5 +31,5 @@ class Administrator(Employee):
         pass
 
 class Secretary (Employee):
-
+    pass
     #List<Observer> observers: Observer
--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')

72f9006e29ad43247faf226f86ad4e22159b9e88
CaioNunes <caiofelipe147@gmail.com>
2017-09-19 22:16:35 -0300

    Create initial tests to register users

---
+++ users/tests/test_register_users.py
@@ -0,0 +1,21 @@
+import hashlib
+import pytest
+
+from django.contrib.auth.models import User
+from django.core import mail
+
+from factories import UserFactory
+from users.models import Employee
+from users.models import Administrator
+from users.models import Secretary
+
+@pytest.mark.django_db
+class TestRegisterUsers:
+
+    def setup(self):
+        self.user1 = UserFactory()
+
+    def test_register_user_get(self,client):
+
+        response = client.get('/users/register/')
+        assert response.status_code == 200

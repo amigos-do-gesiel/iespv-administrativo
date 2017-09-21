546dd70c1ea375fe908a5fbe463ee9b4f108b1c8
ulyssesBML <ulysses3353@gmail.com>
2017-09-21 15:21:44 -0300

    Fixing users.urls problem of token
    Signed-off-by: ulyssesBML <ulysses3353@gmail.com>
    Signed-off-by: GesielFreitas <gesiel.was.f@gmail.com>

--- project/IESPV/users/urls.py
+++ project/IESPV/users/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     
     url(r'^solicitar_senha/', views.solicitation_reset_password, name = 'solicitation_password'),
-    url(r'^recuperar_senha/(?P<token>[0-9a-f]+)/', views.url_recovery, name = 'recovery_password'),
+    url(r'^recuperar_senha/(?P<token>[0-9a-z]+)/', views.url_recovery, name = 'recovery_password'),
 ]

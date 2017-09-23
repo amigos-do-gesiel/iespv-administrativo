7f33a34b86785248e20354a1a1d6646c94a83e41
Marco Kuroiva <marco.antoniorl10@gmail.com>
2017-09-23 02:34:34 -0300

    Redifining views and Administrator's responsibilities

--- users/models.py
+++ users/models.py
@@ -20,8 +20,12 @@ class Employee(User):
 class Administrator(Employee):
     is_superuser = True
 
-    def register_employee():
-        pass
+    def register_employee(employee_type, name, phone_number, email, password):
+        if employee_type == 'secretary':
+            user = create_secretary(name, phone_number, email, password)
+        else:
+            user = create_administrator(name, phone_number, email, password)
+        user.save()
 
     def remove_employee():
         pass
@@ -32,6 +36,19 @@ class Administrator(Employee):
     def block_login():
         pass
 
+    def create_secretary(name, phone_number, email, password):
+        user = Secretary (first_name = name,
+                        phone_number = phone_number,
+                        username = email,
+                        password = password)
+        return user
+
+    def create_administrator(name, phone_number, username, password):
+        user = Administrator (first_name = name,
+                            phone_number = phone_number,
+                            username = email,
+                            password = password)
+        return user
 class Secretary (Employee):
     is_superuser = False
     #List<Observer> observers: Observer
--- users/views.py
+++ users/views.py
@@ -10,15 +10,16 @@ User = get_user_model()
 def index(request):
     return render(request, 'index.html')
 
+#The lines below must be uncommented after the merge
 #@login_required
-#@user_passes_test(lambda user: user.is_staff, login_url='/accounts/dashboard/')
+#@user_passes_test(lambda user: user.is_superuser, login_url='/accounts/dashboard/')
 def register(request):
+    loggedAdmin = request.user
+
     if request.method == "GET":
         return render(request, 'usersRegister/register.html')
     else:
         form = request.POST
-        employee_type = form.get('employee_type')
-
         #in case of error, this variable holds it's message. Otherwise it stays empty
         validation_status = validate_form(form)
 
@@ -28,37 +29,24 @@ def register(request):
                         'usersRegister/register.html',
                         {'falha': validation_status})
 
-
-        if employee_type == 'secretary':
-            user = create_secretary(form)
-        else:
-            user = create_administrator(form)
-
-        user.save()
+        employee_type = form.get('employee_type')
+        name = form.get('name')
+        email = form.get('email')
+        password = form.get('password')
+        loggedAdmin.register_employee(employee_type, name, phone_number, email, password)
 
     return render(request, 'index.html')
 
-def create_secretary(form):
-    user = Secretary (first_name = form.get('name'),
-                    phone_number=form.get('phone_number'),
-                    username=form.get('email'),
-                    password=form.get('password'))
-    return user
-
-def create_administrator(form):
-    user = Administrator (first_name = form.get('name'),
-                        phone_number=form.get('phone_number'),
-                        username=form.get('email'),
-                        password=form.get('password'))
-    return user
 
 def validate_form(form):
+    employee_type = form.get('employee_type')
     name = form.get('name')
     email = form.get('email')
     password = form.get('password')
     confirm_password = form.get('confirmPassword')
 
     resultCheck = ''
+    resultCheck += check_employee_type_selected(employee_type)
     resultCheck += check_name(name)
     resultCheck += check_email(email)
     resultCheck += check_repeated_email(email)
@@ -89,3 +77,11 @@ def check_matching_passwords(password, confirm_password):
         return 'As senhas não combinam  '
     else:
         return ''
+
+def check_employee_type_selected(employee_type):
+    if employee_type == 'secretary':
+        return ''
+    elif employee_type == 'administrator':
+        return ''
+    else:
+        return 'Você deve selecionar o tipo de funcionário  '

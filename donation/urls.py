ca8152cb14ac0237231037f6ffd8da76e4a1d8de
Marco Kuroiva <marco.antoniorl10@gmail.com>
2017-10-14 20:37:44 -0300

    Implementing a donation registration system

--- donation/urls.py
+++ donation/urls.py
@@ -4,4 +4,5 @@ from donation import views
 
 urlpatterns = [
     url(r'^registerdonation/', views.register_donation, name='register_donation'),
+    url(r'^list_donations/', views.list_donations, name='list_donations'),
 ]
--- donation/views.py
+++ donation/views.py
@@ -1,12 +1,44 @@
 from django.shortcuts import render
-from .models import MinMaxFloat, Donation
+from .models import Donation, EquipmentDonation, CashDonation
 from users.models import Employee, Donor, Secretary, Administrator
 
 # authencate later!!!
 def register_donation(request):
-    if request.method == 'GET':
-        donors = Donor.objects.all()
+    donors = Donor.objects.all()
+    employee = request.user
 
+    if request.method == 'GET':
         return render(request, "form_donation.html", {'donors': donors} )
     else:
-        pass
+        form = request.POST
+        donation_type = form.get('donation_type', None)
+        donor_id = form.get('donor', None)
+        value = form.get('value')
+        donor = Donor.objects.filter(id = donor_id)[0]
+        observations = form.get('observations')
+        collection_date = form.get('collection_date')
+        #equipment_id = form.get('equipment', None)
+        #equipment = Equipment.objects.filter(id = equipment_id)[0]
+
+        if donation_type == 'donation_equip':
+            donation_strategy = EquipmentDonation(
+            #                             equipment = equipment
+            )
+        else:
+            donation_strategy = CashDonation(donation_value = value)
+
+        donation_strategy.save()
+        donation = Donation(donor = donor,
+                            employee = employee,
+                            strategy = donation_strategy,
+                            observations = observations,
+                            collection_date = collection_date)
+        donation.save()
+
+    return render(request,"users/index.html")
+
+def list_donations(request):
+    donations = Donation.objects.all()
+
+    if request.method == 'GET':
+        return render(request, "list_donations.html", {'donations': donations} )

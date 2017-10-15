6aa473d797faee15479b9eede7067839c60798d3
gesielsf <gesiel.was.f@gmail.com>
2017-10-15 14:41:42 -0200

    correction in rout of deactivate login secretary

--- users/urls.py
+++ users/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     url(r'^logout/$', views.attendant_logout, name ='logout'),
     url(r'^registerdonor/', views.register_donor, name='register_donor'),
     url(r'^list_secretaries/$', views.listSecretary, name ='list_secretary'),
-    #url(r'^deactive_login_secretary/?P<id_secretary>/', views.deactive_login_secretary, name ='deactive_login_secretary'),
+    url(r'^deactive_login_secretary/(?P<id_secretary>[0-9]+)/', views.deactive_login_secretary, name ='deactive_login_secretary'),
     url(r'^active_login_secretary/(?P<id_secretary>[0-9]+)/', views.active_login_secretary, name ='active_login_secretary'),
 
 ]
